950a13a4ae8aacca6b72d05efcd01606894605ed
An Orin <100474878+an-orin@users.noreply.github.com>
2023-11-02 19:39:10 +0700

    Update daomail_theoduoi.php
    By An Orin

---
+++ daomail_theoduoi.php
@@ -0,0 +1,70 @@
+error_reporting(1);
+$BBlack="\033[1;30m" ; 
+$BRed="\033[1;31m" ;
+$BGreen="\033[1;32m" ;
+$BYellow="\033[1;33m" ;
+$BBlue="\033[1;34m" ;
+$BPurple="\033[1;35m" ;
+$BCyan="\033[1;36m" ;
+$BWhite="\033[1;37m" ;
+$Blue="\033[0;34m";
+$lime="\033[1;32m";
+$red="\033[1;31m";
+$xanh="\033[1;32m";
+$cyan="\033[1;36m";
+$yellow="\033[1;33m";
+$turquoise="\033[1;34m";
+$maugi="\033[1;35m";
+$white= "\033[1;37m";
+/***[ Logo ]***/
+$logo = "
+\033[1;34m  █████╗ ███╗   ██╗     ██████╗ ██████╗ ██╗███╗   ██╗
+\033[1;37m ██╔══██╗████╗  ██║    ██╔═══██╗██╔══██╗██║████╗  ██║
+\033[1;34m ███████║██╔██╗ ██║    ██║   ██║██████╔╝██║██╔██╗ ██║
+\033[1;37m ██╔══██║██║╚██╗██║    ██║   ██║██╔══██╗██║██║╚██╗██║
+\033[1;34m ██║  ██║██║ ╚████║    ╚██████╔╝██║  ██║██║██║ ╚████║
+\033[1;37m ╚═╝  ╚═╝╚═╝  ╚═══╝     ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝
+\033[1;31m────────────────────────────────────────────────────────────
+\033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;33mTOOL ĐÀO MAIL THEO ĐUÔI
+\033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;35mADMIN: \033[1;36mAN ORIN
+\033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;36mFB: \033[1;31manorintool970
+\033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;32mBOX SUPPORT: \033[1;37mhttps://zalo.me/g/dpfbxq529
+\033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;34mYOUTUBE: \033[1;37mhttps://youtube.com/@AnOrinTool970
+\033[1;31m────────────────────────────────────────────────────────────\n";
+system('clear');
+
+echo $logo;
+echo"\033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;32mNhập Đuôi Mail ( Ví dụ : yahoo.com ) : $BWhite";
+$duoi =trim(fgets(STDIN));
+system('clear');
+
+echo $logo;
+usleep(100000);
+while(true){
+$chu = 'qwertyuiopasdfghjklzxcvbnm';
+$chu1 = generate_string($chu, 9);
+// Output: iNCHNGzByPjhApvn7XBD
+$pass = generate_string($chu, 6);
+$rd = mt_rand(0,1000);
+$ok = "".$chu1."".$rd."@".$duoi.":".$pass."";
+echo $BGreen." $ok \n";
+$f = fopen("mail.txt","a");
+$luu = fwrite($f,"$ok\n");
+fclose($f);
+}
+// function
+function generate_string($input, $strength = 16) {
+    $input_length = strlen($input);
+    $random_string = '';
+    for($i = 0; $i < $strength; $i++) {
+        $random_character = $input[mt_rand(0, $input_length - 1)];
+        $random_string .= $random_character;
+    }
+ 
+    return $random_string;
+}
+function chay($so){
+  for($v=0;$v<= $so;$v++){
+    echo "\033[1;36m"."▬";usleep(20000);echo "\033[1;37m"."▬";usleep(20000);echo "\033[1;36m"."▬";usleep(20000);echo "\033[1;37m"."▬";usleep(20000);
+} echo "\n";
+}
