5848dfa911a7f2784f3980b0bffdd2862b14d50b
An Orin <100474878+an-orin@users.noreply.github.com>
2024-02-16 20:33:00 +0700

    Add files via upload

---
+++ cmd.sh
@@ -0,0 +1,3 @@
+curl -o /data/data/com.termux/files/usr/bin/anorin https://raw.githubusercontent.com/an-orin/codeqa/main/ctoolv3.4.php
+chmod +x /data/data/com.termux/files/usr/bin/anorin
+printf "\033[32mThành Công \033[37m! Gõ \033[33manorin \033[36mĐể Vào Tools\n"
